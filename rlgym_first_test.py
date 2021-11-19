ce698345e7b5cf0d4aa0e66571b3cd6e5d847f8f
Gab_C_20 <gabrielsc1618@gmail.com>
2021-11-19 09:07:36 -0300

    new discrete bin size vector

--- rlgym_first_test.py
+++ rlgym_first_test.py
@@ -1,4 +1,5 @@
 import rlgym
+import numpy as np
 from time import time
 
 env = rlgym.make()
@@ -42,26 +43,28 @@ obs_space_low = [-1.7808, -2.2260, 0.0,
                   -1.7507, -1.7507, -1.7507]
 
 
+discrete_num_bins = [100] * len(obs_space_high)
+discrete_bin_size = (np.array(obs_space_high) - np.array(obs_space_low)) / np.array(discrete_num_bins)
+print(discrete_bin_size)
 
 
 
 
-
-while True:
-    obs = env.reset()
-    done = False
-    start_time = time()
-    while not done:
-      #Here we sample a random action. If you have an agent, you would get an action from it here.
-      action = env.action_space.sample() 
-      print(f'obs: {obs[18]}')
-    #   print(f'action: {action}')
+# while True:
+#     obs = env.reset()
+#     done = False
+#     start_time = time()
+#     while not done:
+#       #Here we sample a random action. If you have an agent, you would get an action from it here.
+#       action = env.action_space.sample() 
+#       print(f'obs: {obs[18]}')
+#     #   print(f'action: {action}')
       
-      next_obs, reward, done, gameinfo = env.step(action)
+#       next_obs, reward, done, gameinfo = env.step(action)
       
-      obs = next_obs
-    end_time = time()
-    print(f'Duration: {end_time - start_time}')
-    break
-env.close()
+#       obs = next_obs
+#     end_time = time()
+#     print(f'Duration: {end_time - start_time}')
+#     break
+# env.close()
       
