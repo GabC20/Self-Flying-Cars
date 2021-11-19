c0dace34a208f319e8db962e698d4171bccc05cc
Gab_C_20 <gabrielsc1618@gmail.com>
2021-11-19 08:58:32 -0300

    saved the code with the updated values of obs high and low

--- rlgym_first_test.py
+++ rlgym_first_test.py
@@ -3,52 +3,43 @@ from time import time
 
 env = rlgym.make()
 
-# """
-#     obs_space: [ball.position,
-#                 ball.linear_velocity,
-#                 ball.angular_velocity,
-#                 player_car.position,
-#                 player_car.linear_velocity,
-#                 player_car.angular_velocity,
-#                 player.boost_amount,
-#                 player.has_flip,
-#                 player.on_ground,
-#                 car_data.position,
-#                 car_data.linear_velocity,
-#                 car_data.angular_velocity
-#     ]
-#     obs_space_high = [4096, 5120, 2044,
-#                       6000, 6000, 6000,
-#                       6.0, 6.0, 6.0,
-#                       4096, 5120, 2044,
-#                       2300, 2300, 2300,
-#                       5.5, 5.5, 5.5,
-#                       100.0, 1.0, 1.0,
-#                       4096, 5120, 2044,
-#                       2300, 2300, 2300,
-#                       5.5, 5.5, 5.5
-#     ]
-#     obs_space_low = [-4096, -5120, 0,
-#                      -6000, -6000, -6000,
-#                      -6.0, -6.0, -6.0,
-#                       -4096, -5120, 0,
-#                       -2300, -2300, -2300,
-#                       -5.5, -5.5, -5.5,
-#                       0.0, 0.0, 0.0,
-#                       -4096, -5120, -2044,
-#                       -2300, -2300, -2300,
-#                       -5.5, -5.5, -5.5
-#     ]
+"""
+    obs_space: [ball.position,
+                ball.linear_velocity,
+                ball.angular_velocity,
+                player_car.position,
+                player_car.linear_velocity,
+                player_car.angular_velocity,
+                player.boost_amount,
+                player.has_flip,
+                player.on_ground,
+                car_data.position,
+                car_data.linear_velocity,
+                car_data.angular_velocity
+    ]
+"""
 
-# """
+obs_space_high = [1.7808, 2.2260, 0.8886,
+                  2.6086, 2.6086, 2.6086,
+                  1.9098, 1.9098, 1.9098,
+                  1.7808, 2.2260, 0.8886,
+                  2.6086, 2.6086, 2.6086,
+                  1.7507, 1.7507, 1.7507,
+                  1.0, 1.0, 1.0,
+                  1.7808, 2.2260, 0.8886,
+                  2.6086, 2.6086, 2.6086,
+                  1.7507, 1.7507, 1.7507]
 
-# obs_space_high = [1.7808, 2.2260, 0.8886,
-#                   2.6086, 2.6086, 2.6086,
-#                   1.9098, 1.9098, 1.9098,
-#                   1.7808, 2.2260, 0.8886,
-#                   2.6086, 2.6086, 2.6086,
-#                   1.7507, 1.7507, 1.7507,
-#                   ]
+obs_space_low = [-1.7808, -2.2260, 0.0,
+                  -2.6086, -2.6086, -2.6086,
+                  -1.9098, -1.9098, -1.9098,
+                  -1.7808, -2.2260, 0.0,
+                  -2.6086, -2.6086, -2.6086,
+                  -1.7507, -1.7507, -1.7507,
+                  0.0, 0.0, 0.0,
+                  -1.7808, -2.2260, 0.0,
+                  -2.6086, -2.6086, -2.6086,
+                  -1.7507, -1.7507, -1.7507]
 
 
 
