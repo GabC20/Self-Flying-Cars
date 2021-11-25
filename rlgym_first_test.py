5190de768d7706b2f7b6b43eec69cf6d31301abd
Gab_C_20 <gabrielsc1618@gmail.com>
2021-11-25 08:10:34 -0300

    added fork from rlgym rocket league repo

--- rlgym_first_test.py
+++ rlgym_first_test.py
@@ -47,24 +47,25 @@ discrete_num_bins = [100] * len(obs_space_high)
 discrete_bin_size = (np.array(obs_space_high) - np.array(obs_space_low)) / np.array(discrete_num_bins)
 print(discrete_bin_size)
 
+q_table = np.random.uniform(low=-2, high=2)
 
 
-
-# while True:
-#     obs = env.reset()
-#     done = False
-#     start_time = time()
-#     while not done:
-#       #Here we sample a random action. If you have an agent, you would get an action from it here.
-#       action = env.action_space.sample() 
-#       print(f'obs: {obs[18]}')
-#     #   print(f'action: {action}')
+while True:
+    obs = env.reset()
+    done = False
+    start_time = time()
+    while not done:
+      #Here we sample a random action. If you have an agent, you would get an action from it here.
+      action = env.action_space.sample() 
       
-#       next_obs, reward, done, gameinfo = env.step(action)
+    #   print(f'action: {action}')
       
-#       obs = next_obs
-#     end_time = time()
-#     print(f'Duration: {end_time - start_time}')
-#     break
-# env.close()
+      next_obs, reward, done, gameinfo = env.step(action)
+      print(f'reward: {reward}')
+      
+      obs = next_obs
+    end_time = time()
+    print(f'Duration: {end_time - start_time}')
+    break
+env.close()
       
